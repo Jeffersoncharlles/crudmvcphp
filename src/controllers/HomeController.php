ca08ead243977e02a9f95b3829cd18e6ede62bcf
Jeffersoncharlles <contato@jefferdeveloper.com>
2020-12-14 14:02:02 -0300

    threen commit

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -2,15 +2,36 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\models\Test;
 
 class HomeController extends Controller {
 
     public function index() {
-        $this->render('home', ['nome' => 'Jefferson']);
+        $nome = 'Jefferson';
+        $posts =  [
+            ['titulo' => 'Titutlo de Teste 1'],
+            ['titulo' => 'Titutlo de Teste 2'],
+            ['titulo' => 'Titutlo de Teste 3'],
+            ['titulo' => 'Titutlo de Teste 4']
+        ];
+
+        $dados = [
+            'posts' => $posts,
+            'nome' => $nome
+        ];
+
+        
+
+        $this->render('home', $dados);
     }
 
     public function sobre() {
         $this->render('sobre');
+    }
+    public function fotos() {
+        $teste = new Test();
+
+        $this->render('fotos');
     }
 
     public function sobreP($args) {
--- src/routes.php
+++ src/routes.php
@@ -4,5 +4,8 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
+$router->get('/fotos', 'HomeController@fotos');
+
+
 $router->get('/sobre/{nome}', 'HomeController@sobreP');
 $router->get('/sobre', 'HomeController@sobre');
